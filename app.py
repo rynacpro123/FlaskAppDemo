ae79be3f5a2036ed33880c5be6979fc0bb8c2151
rynacpro123 <rynacpro+github@gmail.com>
2023-12-11 21:22:05 -0800

    Checkin

--- app.py
+++ app.py
@@ -8,15 +8,15 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-
-@app.route('/about')
-def about():
-    return render_template("about.html")
-
-
-@app.route('/user/<string:name>/<int:id>')
-def user(name, id):
-    return "User name: " + name + " - id: " + str(id)
+#
+# @app.route('/about')
+# def about():
+#     return render_template("about.html")
+#
+#
+# @app.route('/user/<string:name>/<int:id>')
+# def user(name, id):
+#     return "User name: " + name + " - id: " + str(id)
 
 
 if __name__ == "__main__":
