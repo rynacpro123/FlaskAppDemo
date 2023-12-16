956bdb993a6899311ffcbc305420e9ced6f5f598
rynacpro123 <rynacpro+github@gmail.com>
2023-12-15 21:16:56 -0800

    Checkin

--- BOT/app.py
+++ BOT/app.py
@@ -1,5 +1,14 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from slack_sdk import WebClient
+
+import os
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+from slack_bolt.adapter.flask import SlackRequestHandler
+from slack_bolt import App
+from dotenv import find_dotenv, load_dotenv
+#from flask import Flask, request
+from functions import draft_email
 
 flask_app = Flask(__name__)
 
