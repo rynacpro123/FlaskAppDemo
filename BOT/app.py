34d827a495fc66a90e07374fe74d03c627d6f415
rynacpro123 <rynacpro+github@gmail.com>
2023-12-15 21:44:20 -0800

    Checkin

--- BOT/app.py
+++ BOT/app.py
@@ -8,6 +8,19 @@ from slack_bolt import App
 from dotenv import find_dotenv, load_dotenv
 from functions import draft_email
 
+
+# Load environment variables from .env file
+load_dotenv(find_dotenv())
+
+# Set Slack API credentials
+SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
+SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
+SLACK_BOT_USER_ID = os.environ["SLACK_BOT_USER_ID"]
+
+# Initialize the Slack app
+app = App(token=SLACK_BOT_TOKEN)
+
+
 flask_app = Flask(__name__)
 
 
