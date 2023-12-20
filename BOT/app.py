fda8fac1785bc987a320bac72bda06b0ff041a8e
rynacpro123 <rynacpro+github@gmail.com>
2023-12-19 21:46:09 -0800

    Checkin

--- BOT/app.py
+++ BOT/app.py
@@ -7,6 +7,9 @@ from slack_bolt.adapter.flask import SlackRequestHandler
 from slack_bolt import App
 from dotenv import find_dotenv, load_dotenv
 from functions import draft_email
+import openai
+
+
 
 
 # Load environment variables from .env file kickddddd
@@ -16,10 +19,11 @@ load_dotenv()
 # SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 # SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
 # SLACK_BOT_USER_ID = os.environ["SLACK_BOT_USER_ID"]
-# OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 
-# Initialize the Slack app
-# app = App(token=SLACK_BOT_TOKEN)
+openai.api_key = OPENAI_API_KEY
+
+
 
 
 flask_app = Flask(__name__)
@@ -31,6 +35,42 @@ MyPracticeKey = os.environ['MyPracticeKey']
 def index():
     return render_template("index.html", MyPracticeKey=MyPracticeKey)
 
+
+# Define the Chatbot route
+@flask_app.route("/chatbot", methods=["POST"])
+def chatbot():
+    #get the message input from the user
+    user_input = request.form['message']
+    #use the OpenAI API to generate a response
+    prompt = f"User: {user_input}\nChatbot: "
+    chat_history = []
+    response = openai.completions.create(
+        model="text-davinci-002",
+        prompt=prompt,
+        temperature=0.5,
+        max_tokens=60,
+        top_p=1,
+        frequency_penalty=0,
+        stop=["\nUser: ","\nChatbot: "]
+
+    )
+
+    #extract the response text from the OpenAI API result
+    bot_response = response.choice[0].text.strip()
+
+
+    #Add the user input and bot response to the chat history
+    chat_history.append(f"User: {user_input}\nChatbot: {bot_response}" )
+
+    #Render the chatbot template with the response text
+    return render_template(
+        "chatbot.html",
+        user_input=user_input,
+        bot_response=bot_response,
+    )
+
+
+
 #
 # @app.route('/about')
 # def about():
@@ -41,6 +81,6 @@ def index():
 # def user(name, id):
 #     return "User name: " + name + " - id: " + str(id)
 
-#
+#start the flask app
 if __name__ == "__main__":
-    flask_app.run(host="0.0.0.0", port=8000)
+    flask_app.run(debug=True, host="0.0.0.0", port=8000)
