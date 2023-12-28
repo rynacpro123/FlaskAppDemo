ee46ab9a9458db8a15b865e9abc7cd91270c73d0
rynacpro123 <rynacpro+github@gmail.com>
2023-12-28 14:52:16 -0800

    Winston 2.0

--- BOT/app.py
+++ BOT/app.py
@@ -1,34 +1,30 @@
 from flask import Flask, render_template, url_for, request
-from slack_sdk import WebClient
 import os
-from slack_sdk import WebClient
-from slack_sdk.errors import SlackApiError
-from slack_bolt.adapter.flask import SlackRequestHandler
-from slack_bolt import App
 from dotenv import find_dotenv, load_dotenv
-from functions import draft_email
 import openai
 
 
 
 
-# Load environment variables from .env file kickddddd
+# Load environment variables from .env file (kickd)
 load_dotenv()
 
-# Set Slack API credentials
-# SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
-# SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
-# SLACK_BOT_USER_ID = os.environ["SLACK_BOT_USER_ID"]
+#setOpenAI key
 OPENAI_API_KEY = os.environ["OPENAIAPIKEY"]
-
 openai.api_key = OPENAI_API_KEY
 
 
 
 
+
+#initialize flask app
 flask_app = Flask(__name__)
 
-#MyPracticeKey = os.environ['MyPracticeKey']
+
+#create initial message list containing system directive
+messages = [{"role": "system", "content": "you are a very extremly cranky and antagonistic and angry old man that is also a helpful assistant. Your name is Kyle Bybee. You live in Utah but plan to move to Bellingham someday. Your going to be very wealthy someday."}]
+
+
 
 @flask_app.route('/')
 @flask_app.route('/home')
@@ -39,36 +35,42 @@ def index():
 # Define the Chatbot route
 @flask_app.route("/chatbot", methods=["POST"])
 def chatbot():
-    #get the message input from the user
-    user_input = request.form['message']
-    #use the OpenAI API to generate a response
-    prompt = f"User: {user_input}\nChatbot: "
-    chat_history = []
-    response = openai.completions.create(
-        model="text-davinci-002",
-        prompt=prompt,
-        temperature=0.5,
-        max_tokens=60,
-        top_p=1,
-        frequency_penalty=0,
-        stop=["\nUser: ","\nChatbot: "]
 
-    )
+    while True:
 
-    #extract the response text from the OpenAI API result
-    bot_response = response.choices[0].text.strip()
+        #obtain the message value input by the user in the message form
+        user_input = request.form['message']
+
+        #append the human response to our input messages list for future calls to openai
+        messages.append({"role": "user", "content": user_input})
+
+        #call openai and pass along message list containing all system, assistance and user inputs within the messages list
+        response = openai.chat.completions.create(
+            model="gpt-3.5-turbo",
+            temperature=0.9,
+            top_p=0.6,
+            max_tokens=200,
+            messages=messages
+        )
+
+        #extract the response text from the OpenAI API result
+        #bot_response = response.choices[0].text.strip()
+        #bot_response = response.choices[0].finish_reason
+        bot_response = response.choices[0].message.content
 
 
 
-    #Add the user input and bot response to the chat history
-    chat_history.append(f"User: {user_input}\nChatbot: {bot_response}" )
 
-    #Render the chatbot template with the response text
-    return render_template(
-        "chatbot.html",
-        user_input=user_input,
-        bot_response=bot_response,
-    )
+        #append the bot response to our input messages list for future calls to openai
+        messages.append({"role": "assistant", "content": bot_response})
+
+
+        #Render the chatbot template with the response text
+        return render_template(
+            "chatbot.html",
+            user_input=user_input,
+            bot_response=bot_response,
+        )
 
 
 
